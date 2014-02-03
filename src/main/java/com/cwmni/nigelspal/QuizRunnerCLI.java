1ac5d9145c8e428705a97bc2a7a2df78159d4a20
William Griffiths <william@ycymro.com>
2014-02-03 01:54:05 +0000

    Javadoc update

--- src/main/java/com/cwmni/nigelspal/QuizRunnerCLI.java
+++ src/main/java/com/cwmni/nigelspal/QuizRunnerCLI.java
@@ -5,6 +5,10 @@ package com.cwmni.nigelspal;
  */
 public final class QuizRunnerCLI
 {
+    /**
+     * Run Quiz 
+     * @param args - required arguments -user <me@google.com> -password <password123> -questions <10> 
+     */
     public static void main(String[] args)
     {
         QuizSettings theQuizSettings = new QuizRunnerCLIPasser(args).getQuizSettings();
